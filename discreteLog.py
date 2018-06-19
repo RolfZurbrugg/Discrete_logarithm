eb9258363fabb320e09e32e7719a270e3752f09c
RolfZurbrugg <rolf.zurbrugg@gmail.com>
2018-06-20 00:00:41 +0200

    Fixed range issue with big step loop
    implemented function to calculate the discreet power correctly

--- discreteLog.py
+++ discreteLog.py
@@ -6,11 +6,25 @@ import math
     49 = 5 ^ 70 mod 167
 '''
 
+def calculate_descreet_power(num: int, exp: int, mod:int) -> int:
+    ans = 1
+    for _ in range(1, exp+1):
+        ans = (ans * num) % mod
+    return ans
+
+ans = calculate_descreet_power(439, 23425, 104729)
+print('expected result = {}'.format(23425))
+
+generator   = 439
+mod_num     = 104729
+Q           = 100000
+
 # user input of variabls
 # ans = generator^x % mod_num
-generator = int(input('Please enter the generator: '))
-mod_num       = int(input('Please enter the modolo: '))
-ans       = int(input('Please enter the answer: '))
+# generator = int(input('Please enter the generator (5): '))
+# mod_num   = int(input('Please enter the modolo (167, 49943): '))
+# ans       = int(input('Please enter the answer (49): '))
+# Q         = int(input('Please enter the maximum value that should be suported: '))
 
 print(generator, mod_num, ans)
 
@@ -19,11 +33,11 @@ print(generator, mod_num, ans)
 # Q = |N^0.5| <- rounded up , N = mod_num -1
 # {(ans * generator^x, x): 0 <= x <= Q - 1}
 
-Q = math.ceil(math.sqrt(mod_num-1))  # calculate sqrt of mod_num - 1 and then round up the result to the next int.
+# Calculating Q instead of having a predefined search space can lead to very long computations or even the program crashing.
+# Q = math.ceil(math.sqrt(mod_num-1))  # calculate sqrt of mod_num - 1 and then round up the result to the next int.
 
 baby_step_dic = {}
-# calculating step 0
-step = ans % mod_num
+step = ans % mod_num  # calculating step 0
 baby_step_dic[step] = 0
 
 for i in range(1, Q):
@@ -31,24 +45,15 @@ for i in range(1, Q):
     baby_step_dic[step] = i
 
 
-# sorted(baby_step_dic) # todo not sure if this is necesary
-print(baby_step_dic)
-
-
 # calculate the giant step list
-# step = (math.pow(generator,Q) % mod_num)  # causes Overflow for large numbers
-step = generator
-for _ in range(2,Q):
-    step = step * generator % mod_num
-
+step = calculate_descreet_power(generator, Q, mod_num)
 step0 = step
-
 solution = -1
 
 if step in baby_step_dic:
     solution = 1*Q-baby_step_dic[step]
 else:
-    for i in range(2, Q*Q):
+    for i in range(2, Q+1):
         step = step * step0 % mod_num
         if step in baby_step_dic:
             solution = i*Q-baby_step_dic[step]
